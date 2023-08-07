3f3eb77c43ff63a9bd9503f27b6a0bc01d580a18
shimatani <deaman6948@gmail.com>
2023-08-07 22:00:24 +0900

    ブランチ切る作業完了

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module SampleApp
     config.load_defaults 6.1
     config.time_zone = 'Asia/Tokyo'
     
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
