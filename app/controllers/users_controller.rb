540a005bca31ebc94fe1b6e8961d4756a200b193
shimatani <deaman6948@gmail.com>
2023-08-09 13:19:18 +0900

    ユーザー登録画面完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,30 @@
 class UsersController < ApplicationController
 
-def new
-  
-end
+  def show
+    @user = User.find(params[:id])
+  end
 
-def create
-  @user = User.find(params[:id])
-end
+  def new
+    @user = User.new
+  end
 
-def show
-  @user = User.find(params[:id])
-end
+  def create
+    @user = User.new(user_params)
+      if @user.save
+      else
+        render:new
+      end
+  end
 
-def updated
-end
+  def updated
+  end
 
-def destroy
-end
+  def destroy
+  end
+
+  private
+   
+   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confiramation)
+   end
 end
